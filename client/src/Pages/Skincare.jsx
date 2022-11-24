d64c2df74795a4a718e87c8aa7c9e48869736d91
Shakti Modak <shaktimodak81@gmail.com>
2022-11-24 17:52:12 +0530

    solved

--- client/src/Pages/Skincare.jsx
+++ client/src/Pages/Skincare.jsx
@@ -5,16 +5,24 @@ import React, { useEffect, useState } from 'react'
 import { GoChevronDown } from 'react-icons/go';
 import { Link } from 'react-router-dom';
 import { MdKeyboardArrowRight } from 'react-icons/md';
+import { AiFillCloseCircle } from 'react-icons/ai';
 import {
   Menu,
   MenuButton,
   MenuList,
   MenuItemOption,
-
+  Checkbox, CheckboxGroup ,
   Button,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Radio
 
 } from '@chakra-ui/react'
 import ProductBox from '../Components/ProductBox/ProductBox';
+import { FiCheckCircle } from 'react-icons/fi';
 
 function Skincare() {
   const breakpoints = {
@@ -80,7 +88,7 @@ function Skincare() {
       <Flex flexDirection={{lg:'row',md:'column',sm:'column'}}>
         <Box display='flex' flexDirection='column' w={{lg:'25%',md:'35%',sm:'50%'}} h='min-content' m='15px' gap='10px'  >
           <Box  >
-            <Menu matchWidth='true'>
+            {/* <Menu matchWidth='true'>
               <MenuButton as={Button} bg='white' w='80%' >
                 <Flex >
                   <Flex justifyContent='flex-start' gap='10px' >
@@ -96,10 +104,49 @@ function Skincare() {
                 <MenuItemOption onClick={() => changeUrlOrder("asc")}>Price:Low To High</MenuItemOption>
                 <MenuItemOption onClick={() => changeUrlOrder("desc")}>Price:Hight To Low</MenuItemOption>
               </MenuList>
-            </Menu>
+            </Menu> */}
+
+
+
+
+
+<Accordion defaultIndex={[0]} allowMultiple bg={"white"} width="80%"  margin={"auto"} borderRadius={"10"} >
+ 
+ 
+ 
+ 
+   <AccordionItem isFocusable="false"  >
+ 
+     
+     <h2 >
+       <AccordionButton _hover={{background:"white", borderRadius:"10"}} >
+         <Box flex='1' fontWeight={"bold"} textAlign='left'>
+         Sort By: Relevance
+         </Box>
+         <Text marginRight={"10px"}><AiFillCloseCircle /></Text>
+         <AccordionIcon />
+       </AccordionButton>
+     </h2>
+     <AccordionPanel textAlign={"left"} display="block" >
+     <Radio value='1'>
+      Low to Hight
+    </Radio><br />
+    <Radio  value='1'>
+       Hight to Low
+    </Radio><br />
+     
+         
+ 
+     </AccordionPanel>
+   </AccordionItem>
+ </Accordion>
+
+
+
+
           </Box>
           <Box >
-            <Menu matchWidth='true'>
+            {/* <Menu matchWidth='true'>
               <MenuButton as={Button} bg='white' w='80%' >
                 <Flex >
                   <Flex justifyContent='flex-start' gap='10px' >
@@ -111,15 +158,79 @@ function Skincare() {
                 </Flex>
 
               </MenuButton>
-              <MenuList >
+              <MenuList > */}
                
-                <MenuItemOption onClick={() => changeUrlType("mask")}>Face Mask</MenuItemOption>
+                {/* <MenuItemOption onClick={() => changeUrlType("mask")}>Face Mask</MenuItemOption>
                 <MenuItemOption onClick={() => changeUrlType("acne")}>Acne Care </MenuItemOption>
                 <MenuItemOption onClick={() => changeUrlType("moisturizer")}>Face Cream</MenuItemOption>
                 <MenuItemOption onClick={() => changeUrlType("serum")}>Face Serum</MenuItemOption>
-                <MenuItemOption onClick={() => changeUrlType("")}>Reset Filters</MenuItemOption>
-              </MenuList>
-            </Menu>
+                <MenuItemOption onClick={() => changeUrlType("")}>Reset Filters</MenuItemOption> */}
+
+
+
+<Accordion defaultIndex={[0]} allowMultiple bg={"white"} width="80%"  margin={"auto"} borderRadius={"10"} >
+ 
+
+<AccordionItem isFocusable="false" border="none"  >
+
+    
+<h2 >
+  <AccordionButton _hover={{background:"white", borderRadius:"10"}} gap={5}>
+    <Box flex='1' fontWeight={"bold"} textAlign='left'>
+     Filtrs
+    </Box>
+    <Text fontSize={"15px"} color={"red"}>Reset</Text>
+  </AccordionButton>
+</h2>
+
+</AccordionItem>
+
+
+
+  <AccordionItem isFocusable="false"  >
+
+    
+    <h2 >
+      <AccordionButton _hover={{background:"white", borderRadius:"10"}} >
+        <Box flex='1' fontWeight={"bold"} textAlign='left'>
+          Product Type
+        </Box>
+        <Text marginRight={"13px"} fontSize={"10px"} color={"red"}>Clear All</Text>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel textAlign={"left"} display="block" >
+    <Checkbox defaultChecked>Face Mask</Checkbox><br />
+    <Checkbox defaultChecked>Acne Care</Checkbox><br />
+    <Checkbox defaultChecked>Face Cream</Checkbox><br />
+    <Checkbox defaultChecked>Face Serum</Checkbox><br />
+    
+        
+
+    </AccordionPanel>
+  </AccordionItem>
+</Accordion>
+
+              {/* </MenuList>
+            </Menu> */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
           </Box>
         </Box>
         <Grid display='grid' templateColumns={{ lg:'repeat(3,1fr)',md:'repeat(2,1fr)',sm:'repeat(1,1fr)'}} w='75%' m='10px'>
